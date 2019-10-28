b2e701e774bd8968c41c949ffca0807e6a279b63
Kazybek Askarbek <kazybekaskarbek@innopolis.university>
2019-10-28 20:00:17 +0300

    model and view added

---
+++ graph.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+
+class Graph():
+    def __init__(self, y0=0, x0=1, X=8, h=5):
+        self.y0 = y0  # initial value for y
+        self.x0 = x0  # initial value for x
+        self.X = X  # final value for x
+        self.h = h  # step
+
+        self.exact_list = []  # exact solution
+        self.euler_list = []  # euler method
+        self.imp_euler_list = []  # improved euler method
+        self.rk_list = []  # runge-kutta
+
+        self.le_euler = []  # local error of euler method
+        self.le_imp_euler = []  # local error of improved euler method
+        self.le_rk = []  # local error of runge-kutta method
+
+    # diff. eq. y' = y/x - xe^(y/x)
+    def y_prime(self, x, y):
+        return y / x - x * (pow(np.e, (y / x)))
+
+    def exact_solution(self, x):
+        return (-x) * np.log(x)
+
+    def calc_exact(self):
+        self.exact_list = []
+        for i in np.arange(self.x0, self.X + self.h, self.h):
+            self.exact_list.append(self.exact_solution(i))
+
+        # Euler method y_n = y_n-1 + h*f(n-1, y_n-1)
+        # Improved Euler method y_n = y_n-1 + h/2 * (f(n-1, y_n-1) + f(n, y_n-1 + h*f(n-1, y_n-1)))
+        # Runge-Kutta method y_n =
+
+    def init_lists(self):
+        self.euler_list = []
+        self.imp_euler_list = []
+        self.rk_list = []
+        self.le_euler = []
+        self.le_imp_euler = []
+        self.le_rk = []
+
+        self.euler_list.append(self.y0)
+        self.imp_euler_list.append(self.y0)
+        self.rk_list.append(self.y0)
+
+        self.le_euler.append(0)
+        self.le_imp_euler.append(0)
+        self.le_rk.append(0)
+
+    def calculate(self):
+
+        self.init_lists()
+
+        for i in np.arange(self.x0 + self.h, self.X + self.h, self.h):
+            e = self.euler_list[-1] + self.h * self.y_prime(i - self.h, self.euler_list[-1])
+
+            i_e = self.imp_euler_list[-1] + 0.5 * self.h * (self.y_prime(i - self.h, self.imp_euler_list[-1])
+                                                            + self.y_prime(i - self.h, self.imp_euler_list[-1]
+                                                                           + self.h * self.y_prime(i - self.h,
+                                                                                                   self.imp_euler_list[
+                                                                                                       -1])))
+
+            k1 = self.y_prime(i - self.h, self.rk_list[-1])
+            k2 = self.y_prime(i - self.h / 2, self.rk_list[-1] + (self.h * k1) / 2)
+            k3 = self.y_prime(i - self.h / 2, self.rk_list[-1] + (self.h * k2) / 2)
+            k4 = self.y_prime(i, self.rk_list[-1] + self.h * k3)
+            rk = self.rk_list[-1] + (self.h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+            exact = self.exact_solution(i)
+
+            self.euler_list.append(e)
+            self.imp_euler_list.append(i_e)
+            self.rk_list.append(rk)
+
+            self.le_euler.append(abs(exact - e))
+            self.le_imp_euler.append(abs(exact - i_e))
+            self.le_rk.append(abs(exact - rk))
+
+            # Blue-le_euler, Orange-le_imp_euler, Green-le_rk
+            #
+            # df = pd.DataFrame(zip(ax, euler_list, imp_euler_list, rk_list, exact_list,
+            #                       [h for i in np.arange(x0, X + h, h)], le_euler, le_imp_euler, le_rk),
+            #                   columns=['x', 'euler', 'imp_euler', 'runge-kutta', 'exact', 'h',
+            #                            'le euler', 'le imp euler', 'le runge-kutta'])
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-  import sys
+import sys
 from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QGridLayout
-
-import pandas as pd
+from graph import Graph
 import matplotlib.pyplot as plt
-import numpy as np
-
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
 
 
 class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
+
+        # Model, from MVC design pattern
+        self.graph = Graph()
 
         # a figure instance to plot on
         self.figure_euler = plt.figure()
@@ -51,6 +50,12 @@ class Window(QDialog):
         self.setLayout(layout)
 
     def plot(self):
+
+        # Calculate
+        self.graph.calculate()
+        self.graph.calc_exact()
+
+        # clear all figures
         self.figure_euler.clear()
         self.figure_imp_euler.clear()
         self.figure_rk.clear()
@@ -63,10 +68,10 @@ class Window(QDialog):
         ax_exact = self.figure_exact.add_subplot(111)
 
         # plot data
-        ax_euler.plot(euler_list)
-        ax_imp_euler.plot(imp_euler_list)
-        ax_rk.plot(rk_list)
-        ax_exact.plot(exact_list)
+        ax_euler.plot(self.graph.euler_list, color="blue")
+        ax_imp_euler.plot(self.graph.imp_euler_list, color="magenta")
+        ax_rk.plot(self.graph.rk_list, color="red")
+        ax_exact.plot(self.graph.exact_list, color="black")
 
         # refresh canvas
         self.canvas_euler.draw()
@@ -74,107 +79,8 @@ class Window(QDialog):
         self.canvas_rk.draw()
         self.canvas_exact.draw()
 
-# diff. eq. y' = y/x - xe^(y/x)
-def y_prime(x, y):
-    return y / x - x * (pow(np.e, (y / x)))
-
-
-def exact_solution(x):
-    return (-x) * np.log(x)
-
-
-exact_list = []  # exact solution
-euler_list = []  # euler method
-imp_euler_list = []  # improved euler method
-rk_list = []  # runge-kutta
-
-le_euler = []  # local error of euler method
-le_imp_euler = []  # local error of improved euler method
-le_rk = []  # local error of runge-kutta method
-
-
-def compute_lists(X0, Y0, X, step):
-    list_exact_solution(X0, X, step)
-    list_approximated_solution(X0, Y0, X, step)
-
-
-def list_exact_solution(X0, N, H):
-    global exact_list
-    exact_list = []
-    for i in np.arange(X0, N + H, H):
-        exact_list.append(exact_solution(i))
-
-
-def list_approximated_solution(X0, Y0, N, H):
-    # Euler method y_n = y_n-1 + h*f(n-1, y_n-1)
-    # Improved Euler method y_n = y_n-1 + h/2 * (f(n-1, y_n-1) + f(n, y_n-1 + h*f(n-1, y_n-1)))
-    # Runge-Kutta method y_n =
-
-    global euler_list
-    global imp_euler_list
-    global rk_list
-
-    global le_euler
-    global le_imp_euler
-    global le_rk
-
-    euler_list = []
-    imp_euler_list = []
-    rk_list = []
-
-    le_euler = []
-    le_imp_euler = []
-    le_rk = []
-
-    euler_list.append(Y0)
-    imp_euler_list.append(Y0)
-    rk_list.append(Y0)
-
-    le_euler.append(0)
-    le_imp_euler.append(0)
-    le_rk.append(0)
-
-    for i in np.arange(X0 + H, N + H, H):
-        e = euler_list[-1] + H * y_prime(i - H, euler_list[-1])
-        i_e = imp_euler_list[-1] + 0.5 * H * (y_prime(i - H, imp_euler_list[-1])
-                                              + y_prime(i - H, imp_euler_list[-1]
-                                                        + h * y_prime(i - H, imp_euler_list[-1])))
-
-        k1 = y_prime(i - H, rk_list[-1])
-        k2 = y_prime(i - H / 2, rk_list[-1] + (H * k1) / 2)
-        k3 = y_prime(i - H / 2, rk_list[-1] + (H * k2) / 2)
-        k4 = y_prime(i, rk_list[-1] + H * k3)
-        rk = rk_list[-1] + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-
-        exact = exact_solution(i)
-
-        euler_list.append(e)
-        imp_euler_list.append(i_e)
-        rk_list.append(rk)
-
-        le_euler.append(abs(exact - e))
-        le_imp_euler.append(abs(exact - i_e))
-        le_rk.append(abs(exact - rk))
-
-
-# Blue-le_euler, Orange-le_imp_euler, Green-le_rk
-# plt.figure(figsize=[10, 5])
-# plt.plot(ax, le_euler, ax, le_imp_euler, ax, le_rk)
-# plt.show()
-#
-# df = pd.DataFrame(zip(ax, euler_list, imp_euler_list, rk_list, exact_list,
-#                       [h for i in np.arange(x0, X + h, h)], le_euler, le_imp_euler, le_rk),
-#                   columns=['x', 'euler', 'imp_euler', 'runge-kutta', 'exact', 'h',
-#                            'le euler', 'le imp euler', 'le runge-kutta'])
-
 
 if __name__ == '__main__':
-    y0 = 0  # intial value for y
-    x0 = 1  # intial value for x
-    X = 8
-    h = 5  # step
-    compute_lists(x0, y0, X, h)
-
     app = QApplication(sys.argv)
 
     main = Window()
