5f6b66d894c777620cece938fe29e284e2f9be27
Kazybek Askarbek <kazybekaskarbek@innopolis.university>
2019-10-28 23:33:42 +0300

    refactoring

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 
 
 class Window(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, is_graph=True, parent=None):
         super(Window, self).__init__(parent)
+
+        # One window will be for displaying errors and one for approximation graphs
+        self.is_graph = is_graph
 
         # Model, from MVC design pattern
         self.graph = Graph()
 
-        # a figure instance to plot on
-        self.figure_euler = plt.figure()
-        self.figure_imp_euler = plt.figure()
-        self.figure_rk = plt.figure()
-        self.figure_exact = plt.figure()
+        if self.is_graph:
+            # a figure instance to plot on
+            self.figure_euler = plt.figure()
+            self.figure_imp_euler = plt.figure()
+            self.figure_rk = plt.figure()
+            self.figure_exact = plt.figure()
+        else:
+            self.figure_euler_error = plt.figure()
+            self.figure_imp_euler_error = plt.figure()
+            self.figure_rk_error = plt.figure()
 
         # set the layout
         main_layout = QHBoxLayout()
@@ -28,17 +36,27 @@ class Window(QDialog):
 
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
-        self.canvas_euler = FigureCanvas(self.figure_euler)
-        self.canvas_imp_euler = FigureCanvas(self.figure_imp_euler)
-        self.canvas_rk = FigureCanvas(self.figure_rk)
-        self.canvas_exact = FigureCanvas(self.figure_exact)
+        if self.is_graph:
+            self.canvas_euler = FigureCanvas(self.figure_euler)
+            self.canvas_imp_euler = FigureCanvas(self.figure_imp_euler)
+            self.canvas_rk = FigureCanvas(self.figure_rk)
+            self.canvas_exact = FigureCanvas(self.figure_exact)
+        else:
+            self.canvas_euler_error = FigureCanvas(self.figure_euler_error)
+            self.canvas_imp_euler_error = FigureCanvas(self.figure_imp_euler_error)
+            self.canvas_rk_error = FigureCanvas(self.figure_rk_error)
 
         # this is the Navigation widget
         # it takes the Canvas widget and a parent
-        self.toolbar_euler = NavigationToolbar(self.canvas_euler, self)
-        self.toolbar_imp_euler = NavigationToolbar(self.canvas_imp_euler, self)
-        self.toolbar_rk = NavigationToolbar(self.canvas_rk, self)
-        self.toolbar_exact = NavigationToolbar(self.canvas_exact, self)
+        if self.is_graph:
+            self.toolbar_euler = NavigationToolbar(self.canvas_euler, self)
+            self.toolbar_imp_euler = NavigationToolbar(self.canvas_imp_euler, self)
+            self.toolbar_rk = NavigationToolbar(self.canvas_rk, self)
+            self.toolbar_exact = NavigationToolbar(self.canvas_exact, self)
+        else:
+            self.toolbar_euler_error = NavigationToolbar(self.canvas_euler_error, self)
+            self.toolbar_imp_euler_error = NavigationToolbar(self.canvas_imp_euler_error, self)
+            self.toolbar_rk_error = NavigationToolbar(self.canvas_rk_error, self)
 
         # Just some button connected to `plot` method
         self.b_plot = QPushButton('Plot ')
@@ -52,22 +70,27 @@ class Window(QDialog):
         layout.addLayout(glayout)
         layout.addWidget(self.b_plot)
 
-        glayout.addWidget(self.toolbar_euler, 0, 0)
-        glayout.addWidget(self.canvas_euler, 1, 0)
+        if is_graph:
+            glayout.addWidget(self.toolbar_euler, 0, 0)
+            glayout.addWidget(self.canvas_euler, 1, 0)
 
-        glayout.addWidget(self.toolbar_imp_euler, 0, 1)
-        glayout.addWidget(self.canvas_imp_euler, 1, 1)
+            glayout.addWidget(self.toolbar_imp_euler, 0, 1)
+            glayout.addWidget(self.canvas_imp_euler, 1, 1)
 
-        glayout.addWidget(self.toolbar_rk, 2, 0)
-        glayout.addWidget(self.canvas_rk, 3, 0)
+            glayout.addWidget(self.toolbar_rk, 2, 0)
+            glayout.addWidget(self.canvas_rk, 3, 0)
 
-        glayout.addWidget(self.toolbar_exact, 2, 1)
-        glayout.addWidget(self.canvas_exact, 3, 1)
+            glayout.addWidget(self.toolbar_exact, 2, 1)
+            glayout.addWidget(self.canvas_exact, 3, 1)
+        else:
+            glayout.addWidget(self.toolbar_euler_error, 0, 0)
+            glayout.addWidget(self.canvas_euler_error, 1, 0)
 
+            glayout.addWidget(self.toolbar_imp_euler_error, 0, 1)
+            glayout.addWidget(self.canvas_imp_euler_error, 1, 1)
 
-        # glayout.addWidget(self.canvas_imp_euler, 0, 1)
-        # glayout.addWidget(self.canvas_rk, 1, 0)
-        # glayout.addWidget(self.canvas_exact, 1, 1)
+            glayout.addWidget(self.toolbar_rk_error, 2, 0)
+            glayout.addWidget(self.canvas_rk_error, 3, 0)
 
         self.x0 = QLineEdit()
         self.y0 = QLineEdit()
@@ -96,35 +119,57 @@ class Window(QDialog):
         # Calculate
         self.graph.calculate()
 
-        # clear all figures
-        self.figure_euler.clear()
-        self.figure_imp_euler.clear()
-        self.figure_rk.clear()
-        self.figure_exact.clear()
+        if self.is_graph:
+            # clear all figures
+            self.figure_euler.clear()
+            self.figure_imp_euler.clear()
+            self.figure_rk.clear()
+            self.figure_exact.clear()
 
-        # create an axis
-        ax_euler = self.figure_euler.add_subplot(111)
-        ax_imp_euler = self.figure_imp_euler.add_subplot(111)
-        ax_rk = self.figure_rk.add_subplot(111)
-        ax_exact = self.figure_exact.add_subplot(111)
+            # create an axis
+            ax_euler = self.figure_euler.add_subplot(111)
+            ax_imp_euler = self.figure_imp_euler.add_subplot(111)
+            ax_rk = self.figure_rk.add_subplot(111)
+            ax_exact = self.figure_exact.add_subplot(111)
 
-        # plot data
-        ax_euler.plot(self.graph.euler_list, color="blue")
-        ax_imp_euler.plot(self.graph.imp_euler_list, color="magenta")
-        ax_rk.plot(self.graph.rk_list, color="red")
-        ax_exact.plot(self.graph.exact_list, color="black")
+            # plot data
+            ax_euler.plot(self.graph.euler_list, color="blue")
+            ax_imp_euler.plot(self.graph.imp_euler_list, color="magenta")
+            ax_rk.plot(self.graph.rk_list, color="red")
+            ax_exact.plot(self.graph.exact_list, color="black")
 
-        # refresh canvas
-        self.canvas_euler.draw()
-        self.canvas_imp_euler.draw()
-        self.canvas_rk.draw()
-        self.canvas_exact.draw()
+            # refresh canvas
+            self.canvas_euler.draw()
+            self.canvas_imp_euler.draw()
+            self.canvas_rk.draw()
+            self.canvas_exact.draw()
+        else:
+            self.figure_euler_error.clear()
+            self.figure_imp_euler_error.clear()
+            self.figure_rk_error.clear()
+
+            ax_euler_error = self.figure_euler_error.add_subplot(111)
+            ax_imp_euler_error = self.figure_imp_euler_error.add_subplot(111)
+            ax_rk_error = self.figure_rk_error.add_subplot(111)
+
+            # plot data
+            ax_euler_error.plot(self.graph.le_imp_euler, color="blue")
+            ax_imp_euler_error.plot(self.graph.le_imp_euler, color="magenta")
+            ax_rk_error.plot(self.graph.le_rk, color="red")
+
+            # refresh canvas
+            self.canvas_euler_error.draw()
+            self.canvas_imp_euler_error.draw()
+            self.canvas_rk_error.draw()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    main = Window()
-    main.showMaximized()
+    graph = Window(True)
+    error = Window(False)
+
+    graph.showMaximized()
+    error.showMaximized()
 
     sys.exit(app.exec_())
