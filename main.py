2b4fd6ef3625210cfd64e92d6aac707b88213c5a
Kazybek Askarbek <kazybekaskarbek@innopolis.university>
2019-10-28 20:30:12 +0300

    config ivp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QGridLayout, QHBoxLayout, QFormLayout, QLineEdit, QLabel
 from graph import Graph
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -35,19 +35,31 @@ class Window(QDialog):
         self.b_plot.clicked.connect(self.plot)
 
         # set the layout
+        main_layout = QHBoxLayout()
+        config_layout = QFormLayout()
         layout = QVBoxLayout()
-        layout.addWidget(self.toolbar)
-
         glayout = QGridLayout()
+
+        layout.addWidget(self.toolbar)
+        layout.addLayout(glayout)
+        layout.addWidget(self.b_plot)
 
         glayout.addWidget(self.canvas_euler, 0, 0)
         glayout.addWidget(self.canvas_imp_euler, 0, 1)
         glayout.addWidget(self.canvas_rk, 1, 0)
         glayout.addWidget(self.canvas_exact, 1, 1)
 
-        layout.addLayout(glayout)
-        layout.addWidget(self.b_plot)
-        self.setLayout(layout)
+        config_layout.addRow(QLabel('x0:'), QLineEdit())
+        config_layout.addRow(QLabel('y0:'), QLineEdit())
+        config_layout.addRow(QLabel('X:'), QLineEdit())
+        config_layout.addRow(QLabel('h:'), QLineEdit())
+
+        set_button = QPushButton("Set")
+        config_layout.addWidget(set_button)
+        main_layout.addLayout(layout)
+        main_layout.addLayout(config_layout)
+
+        self.setLayout(main_layout)
 
     def plot(self):
 
@@ -87,3 +99,5 @@ if __name__ == '__main__':
     main.showMaximized()
 
     sys.exit(app.exec_())
+
+
